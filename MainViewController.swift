5dce40062e73a20a65f0cf5685ad7ee0640f2a58
Avx-arch <jasminerdgz6@gmail.com>
2026-03-18 03:54:20 +0530

    Create MainViewController.swift

---
+++ MainViewController.swift
@@ -0,0 +1,41 @@
+import UIKit
+import NetworkExtension
+
+class MainViewController: UIViewController {
+    let startButton = UIButton(type: .system)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black // This permanently kills the white screen
+        
+        startButton.setTitle("START 2026 ENGINE", for: .normal)
+        startButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 24)
+        startButton.setTitleColor(.cyan, for: .normal)
+        startButton.layer.borderColor = UIColor.cyan.cgColor
+        startButton.layer.borderWidth = 2
+        startButton.frame = CGRect(x: 0, y: 0, width: 300, height: 70)
+        startButton.center = view.center
+        startButton.addTarget(self, action: #selector(triggerEngine), for: .touchUpInside)
+        
+        view.addSubview(startButton)
+    }
+
+    @objc func triggerEngine() {
+        NETunnelProviderManager.loadAllFromPreferences { managers, _ in
+            let manager = managers?.first ?? NETunnelProviderManager()
+            let config = NETunnelProviderProtocol()
+            
+            config.providerBundleIdentifier = "com.test.ios.extension" 
+            config.serverAddress = "FreeFire-Proxy"
+            
+            manager.protocolConfiguration = config
+            manager.isEnabled = true
+            
+            manager.saveToPreferences { _ in
+                try? manager.connection.startVPNTunnel()
+                self.startButton.setTitle("ENGINE ACTIVE", for: .normal)
+                self.startButton.setTitleColor(.red, for: .normal)
+            }
+        }
+    }
+}
