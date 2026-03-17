1d99cf10c94ca6e8ea9249b9d8ffd6ac3a257e6e
Avx-arch <jasminerdgz6@gmail.com>
2026-03-18 04:01:25 +0530

    Update Services.swift

--- Services.swift
+++ Services.swift
@@ -1,11 +1,21 @@
-//
-//  Services.swift
-//
+import Foundation
 
-struct Services {
-  // static let preloading = PreloadingService()
-  // static let liveData = LiveDataService()
-  // static let globalMessage = GlobalMessageService()
-  // static let dataDownloader = DataDownloaderService()
-  // static let ar = ARService()
+struct UnityVariableNode {
+    var bitSize: UInt32
+    var bitAlign: UInt32
+    var offset: UInt64
+}
+
+class MemoryManager {
+    static func calculateTotalSize(node: UnityVariableNode) -> UInt64 {
+        let size = UInt64(node.bitSize)
+        if node.offset != UInt64.max { 
+            return size + node.offset
+        }
+        return size
+    }
+    
+    static func getAlignment(isPacked: Bool, defaultAlign: UInt32) -> UInt32 {
+        return isPacked ? 1 : defaultAlign
+    }
 }
